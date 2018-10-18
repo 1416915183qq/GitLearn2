b2605949d29b374367bced837a6c6103fbb44aeb
liuluyang <liuluyang0312@163.com>
2018-10-18 20:01:33 +0800

    liuly0312 本地master提交 该段代码 至 remote master 001

--- app/src/main/java/com/boocax/robot/gitlearn2/MainActivity.java
+++ app/src/main/java/com/boocax/robot/gitlearn2/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //liuly0312 本地master提交 该段代码 至 remote master 001
     }
 }
