10f70c9f94b2d794c7ccb3c11f5489e92398306c
liuluyang <liuluyang0312@163.com>
2018-10-19 14:14:59 +0800

    liuly0312 本地master提交 该段代码 至 remote (融合了功能 1 和 2 和 3)

--- app/src/main/java/com/boocax/robot/gitlearn2/MainActivity.java
+++ app/src/main/java/com/boocax/robot/gitlearn2/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //liuly0312 本地master提交 该段代码 至 remote master 001
+
+        //融合了功能1 2 3
+        Fun1 f1 = new Fun1();
+        Fun2 f2 = new Fun2();
+        Fun3 f3 = new Fun3();
+        f1.doSth();
+        f2.doSth();
+        f3.doSth();
     }
 }
