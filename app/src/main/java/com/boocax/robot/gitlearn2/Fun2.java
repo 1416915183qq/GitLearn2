1c183af15f1f5c192de7db05aafb9382080bebd1
liuluyang <liuluyang0312@163.com>
2018-10-19 11:21:23 +0800

    liuly0312 本地master提交 该段代码 至 remote (增加了功能2同时融合了qq开发的功能1)

---
+++ app/src/main/java/com/boocax/robot/gitlearn2/Fun2.java
@@ -0,0 +1,10 @@
+package com.boocax.robot.gitlearn2;
+
+import android.util.Log;
+
+public class Fun2 {
+    public void doSth() {
+        //这是liuly0312 编写的Fun2功能 提交至develop分支
+        Log.d("TAG", "doSth2");
+    }
+}
